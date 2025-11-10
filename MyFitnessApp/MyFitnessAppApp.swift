ba23070edfa505bd506916fa9d163c83cb9f5262
shivsupekar <shivsupekar123@gmail.com>
2025-11-10 21:20:59 +0530

    Initial commit

---
+++ MyFitnessApp/MyFitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFitnessAppApp.swift
+//  MyFitnessApp
+//
+//  Created by Shivam on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
